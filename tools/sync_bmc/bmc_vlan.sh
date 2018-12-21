460f57e94d6954f6981ceeb52ea8e12a7fc8a3d2
Wirut Getbamrung <wgetbum@icloud.com>
2018-12-21 16:05:37 +0700

    Remove ipv6 on eth0.4088 vlan

--- tools/sync_bmc/bmc_vlan.sh
+++ tools/sync_bmc/bmc_vlan.sh
@@ -2,5 +2,5 @@
 
 # Add vlan
 ip link add link eth0 name eth0.4088 type vlan id 4088
-ifconfig eth0.4088 inet6 add "fe80::2:1/64" up
 ip addr add 240.1.1.2/30 dev eth0.4088
+ip link set eth0.4088 up 
